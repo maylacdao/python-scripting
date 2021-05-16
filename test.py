e73a6d68a3cbf82b7e1624abbb7add737bd19405
maylacdao <may.lacdao@gmail.com>
2021-05-16 10:06:57 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,36 @@
-print("Hello World!")
+# Create first variable and print
+msg = "Hello, World!"
+print(msg)
+
+# data types review
+
+integer_ = 3
+print(integer_)
+
+float_ = 3.14
+print(float_)
+
+boolean_ = True
+print(boolean_)
+
+# More on variables
+
+title = "My Great Movie!"
+print(title)
+
+years = 80
+print(years)
+
+expert_status = True
+print(expert_status)
+
+# functions
+
+# 1. print()
+print("The title of my movie is " + title + ".")
+
+# 2. str()
+print("I am " + str(years) + " years old.")
+
+#alternative to str()
+print(f"I am {years} years old.")
