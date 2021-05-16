e26f812d208fec3d4c9d2a6195b2f00437360afd
maylacdao <may.lacdao@gmail.com>
2021-05-16 01:11:54 -0400

    Update pybankMain.py

--- PyBank/pybankMain.py
+++ PyBank/pybankMain.py
@@ -3,3 +3,15 @@ import csv
 import os
 
 # Create filepath
+dataSet = "PyBank/Resources/budget_data.csv"
+path = os.path.join(dataSet)
+
+with open(dataSet, 'r') as input_file:
+    csvreader = csv.reader(input_file, delimiter=",")
+
+    for row in csvreader:
+        print(row)
+
+# Build financial analysis function
+
+
